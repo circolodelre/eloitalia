997a1cc8111fe802d46b8d7023deb9c9ef056dca
Francesco Bianco <info.francescobianco@gmail.com>
2019-02-12 23:23:48 +0100

    asd

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# eloitalia
+# Mechline
+
+## Staging
+
+Check if environment is ready
+```
+ansible -i hosts -m ping staging
+```
+
+Deploy latest version of wordpress
+```
+ansible-playbook -i hosts -l staging deploy.yml 
+```
+
+
+## Production
---
+++ plugin/plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package wp-granular
+ * @version 0.0.1
+ */
+/*
+Plugin Name: WP Granular
+Plugin URI: https://github.com/javanile/wp-granular
+Description: WordPress extension framework based on object-oriented paradigm.
+Author: Francesco Bianco
+Version: 0.0.1
+Author URI: https://github.com/javanile
+*/
+
